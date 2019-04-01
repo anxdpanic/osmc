ce9a424e3272221db070a4cb0b1aa3862c34841c
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-04-01 12:38:10 +0100

    Point to new mirror and leaseweb is giving 404

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -407,9 +407,9 @@ game.libretro.vbam game.libretro.yabause game.libretro.genplus"
         pushd languages
         if [ "$API_VERSION" = "18" ]; then api_name="leia"; fi
         if [ "$API_VERSION" = "19" ]; then api_name="tbc"; fi
-        base_url="http://mirror.us.leaseweb.net/xbmc/addons/${api_name}"
+        base_url="http://mirror.ox.ac.uk/sites/xbmc.org/addons/${api_name}"
 	handle_dep "wget" # We do not usually use wget in the build environment
-        languages=$(wget ${base_url} -O- | grep resource.language. | sed -e 's/<a/\n<a/g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sed '/tr/d' | sed 's/resource.language.//' | tr -d /)
+        languages=$(wget ${base_url} -O- | grep resource.language. | sed -e 's/<a/\n<a/g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sed '/tr/d' | sed 's/resource.language.//' | tr -d / | grep -v 'img src')
         if [ $? != 0 ]; then echo "Can't get list of languages" && exit 1; fi
         for language in ${languages}
         do
