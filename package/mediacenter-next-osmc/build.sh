b26f9c654359ca0439fe741027d7b694ca61ac9f
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-04-02 18:09:23 +0100

    Bump kodi commit

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -14,7 +14,7 @@ API_VERSION="18"
 
 #use xbmc top of tree for vero builds
 else
-pull_source "https://github.com/xbmc/xbmc/archive/9114f656eac11a6b2e7311a3b46cca44a5277112.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/f8100282bc7598649606df58ab4193d95dffb3ff.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 fi
 if [ $? != 0 ]; then echo -e "Error fetching Kodi source" && exit 1; fi
