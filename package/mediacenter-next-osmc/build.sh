3f1e92caa9878f70b09e6bb7bb9d2a156d3d19be
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-05-05 07:10:45 +0100

    Bump nightly build

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -9,7 +9,7 @@ then
 pull_source "https://github.com/popcornmix/xbmc/archive/f557accb02daa604eb1f373cf1e30751d74ad927.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 else
-pull_source "https://github.com/xbmc/xbmc/archive/ae31c4f4e201bb10f725e02a6b56642697a5437a.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/aba6aa94bd4b8c65d3c281b31e70a4c6d0b78a5c.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 
 
