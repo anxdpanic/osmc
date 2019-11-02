32c83e84ddd516aa41afd8ac36e255b4ef2d0b42
Greg McCarthy <greg@gjmccarthy.co.uk>
2019-11-02 08:35:14 +0000

    Bump kodi commit

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -9,7 +9,7 @@ then
 pull_source "https://github.com/popcornmix/xbmc/archive/46cda7db54fb6ef601b02fcf479048a0de9176e2.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 else
-pull_source "https://github.com/xbmc/xbmc/archive/9371b111110f4eeb8a48e1edf2f6de74ae154166.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/cce81198925f2a2ebc0365b42defcda6e678aac6.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 
 
