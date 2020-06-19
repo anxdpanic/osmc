e2279e26ff15eba24f55ee60b1d78784d551445f
Greg McCarthy <greg@gjmccarthy.co.uk>
2020-06-19 19:43:38 +0100

    Point to gbm branch

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp2" ]
 then
-pull_source "https://github.com/popcornmix/xbmc/archive/9208b1a317185b55d067f43914206e88527be229.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/popcornmix/xbmc/archive/1927808270ba6bf0a7be75b7291c17d24758ab2b.tar.gz" "$(pwd)/src"
 API_VERSION="19"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/de0a96056cc301f21b06ef9c76525b16ed62ffb6.tar.gz" "$(pwd)/src"
