53e418b7ef4a7a73fb794125e71e39fe22e48b20
anxdpanic <anxdpanic@gmail.com>
2020-06-27 09:50:56 -0400

    [package][mediacenter-addon-osmc][script.module.osmcsetting.services] Initial pass completed

---
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.services/resources/lib/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['CompLogger', 'service_selection_gui']
--- package/mediacenter-addon-osmc/src/script.module.osmcsetting.services/resources/lib/service_selection_gui.py
+++ package/mediacenter-addon-osmc/src/script.module.osmcsetting.services/resources/lib/service_selection_gui.py
@@ -1,34 +1,37 @@
 import os
 import subprocess
+import sys
 import xbmcaddon
 import xbmcgui
 import xbmc
 from collections import OrderedDict
-import glob
-from CompLogger import comprehensive_logger as clog
+from .CompLogger import comprehensive_logger as clog
 
 ACTION_PREVIOUS_MENU = 10
-ACTION_NAV_BACK      = 92
-SAVE                 = 5
-HEADING              = 1
-ACTION_SELECT_ITEM   = 7
+ACTION_NAV_BACK = 92
+SAVE = 5
+HEADING = 1
+ACTION_SELECT_ITEM = 7
 
-addonid     = "script.module.osmcsetting.services"
-__addon__   = xbmcaddon.Addon(addonid)
+addonid = "script.module.osmcsetting.services"
+__addon__ = xbmcaddon.Addon(addonid)
+
+PY2 = sys.version_info.major == 2
+
 
 def KodiLogger(message):
-
     try:
         message = str(message)
     except UnicodeEncodeError:
-        message = message.encode('utf-8', 'ignore' )
-        
+        message = message.encode('utf-8', 'ignore')
+
     xbmc.log('OSMC SERVICES ' + str(message), level=xbmc.LOGDEBUG)
 
 
-def lang(id):
-    san = __addon__.getLocalizedString(id).encode( 'utf-8', 'ignore' )
-    return san 
+def lang(string_id):
+    if PY2:
+        return __addon__.getLocalizedString(string_id).encode('utf-8', 'ignore')
+    return __addon__.getLocalizedString(string_id)
 
 
 class MaitreD(object):
@@ -36,24 +39,19 @@ class MaitreD(object):
     def __init__(self):
         self.services = {}
 
-
     @clog(KodiLogger)
     def enable_service(self, s_entry):
 
         for service in s_entry:
-        
             os.system("sudo /bin/systemctl enable " + service)
             os.system("sudo /bin/systemctl start " + service)
-    
 
     @clog(KodiLogger)
     def disable_service(self, s_entry):
 
         for service in s_entry:
-        
             os.system("sudo /bin/systemctl disable " + service)
             os.system("sudo /bin/systemctl stop " + service)
-
 
     @clog(KodiLogger)
     def is_running(self, s_entry):
@@ -66,7 +64,6 @@ class MaitreD(object):
                 return False
 
         return True
-
 
     @clog(KodiLogger)
     def is_enabled(self, s_entry):
@@ -81,49 +78,46 @@ class MaitreD(object):
 
         return True
 
-
     @clog(KodiLogger, maxlength=500)
     def discover_services(self):
-        ''' Returns a dict of service tuples. {s_name: (entry, service_name, running, enabled)} 
+        """ Returns a dict of service tuples. {s_name: (entry, service_name, running, enabled)}
             s_name is the name shown in the GUI
             s_entry is the actual service name used to enabling and running, this is a LIST to 
-            handle bundled services '''
+            handle bundled services """
 
         svcs = {}
 
         for service_name in os.listdir("/etc/osmc/apps.d"):
-            service_name = service_name.replace('\n','')
+            service_name = service_name.replace('\n', '')
             if os.path.isfile("/etc/osmc/apps.d/" + service_name):
-                with open ("/etc/osmc/apps.d/" + service_name) as f:
+                with open("/etc/osmc/apps.d/" + service_name) as f:
                     lines = f.readlines()
-                    s_name = lines.pop(0).replace('\n','')
-                    s_entry = [line.replace('\n','') for line in lines]
+                    s_name = lines.pop(0).replace('\n', '')
+                    s_entry = [line.replace('\n', '') for line in lines]
 
                     KodiLogger("MaitreD: Service Friendly Name: %s" % s_name)
                     KodiLogger("MaitreD: Service Entry Point(s): %s" % s_entry)
 
-                    enabled     = self.is_enabled(s_entry)
-                    runcheck    = self.is_running(s_entry)
+                    enabled = self.is_enabled(s_entry)
+                    runcheck = self.is_running(s_entry)
 
                     if runcheck:
                         running = lang(32003)
                     else:
                         running = lang(32005)
 
-                    svcs[s_name] = ( s_entry, service_name, running, enabled )
-                                    
+                    svcs[s_name] = (s_entry, service_name, running, enabled)
 
         # this last part creates a dictionary ordered by the services friendly name
         self.services = OrderedDict()
         for key in sorted(svcs.keys()):
             self.services[key] = svcs[key]
-        
-        return self.services
 
+        return self.services
 
     @clog(KodiLogger, maxlength=250)
     def process_user_changes(self, initiants, finitiants):
-        ''' User selection is a list of tuples (s_name::str, service_name::list, enable::bool) '''
+        """ User selection is a list of tuples (s_name::str, service_name::list, enable::bool) """
 
         for clean_name, s_entry in initiants:
             self.enable_service(s_entry)
@@ -134,22 +128,19 @@ class MaitreD(object):
 
 class service_selection(xbmcgui.WindowXMLDialog):
 
-
     def __init__(self, strXMLname, strFallbackPath, strDefaultName, **kwargs):
 
         # self.service_list = kwargs.get('service_list', [])
 
         # the lists to hold the services to start, and to finish
-        self.initiants  = []
+        self.initiants = []
         self.finitiants = []
 
         self.garcon = MaitreD()
         self.service_list = self.garcon.discover_services()
 
-
     def dummy(self):
         print('logger not provided')
-
 
     def onInit(self):
 
@@ -159,12 +150,12 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
         item_pos = 0
 
-        for s_name, service_tup in self.service_list.iteritems():
+        for s_name, service_tup in self.service_list.items():
             # populate the list
             s_entry, service_name, running, enabled = service_tup
 
             if running != lang(32003):
-                if enabled == True:
+                if enabled is True:
                     sundry = lang(32004)
                 else:
                     sundry = lang(32005)
@@ -185,12 +176,10 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
         self.setFocus(self.name_list)
 
-
     def onAction(self, action):
         actionID = action.getId()
         if (actionID in (ACTION_PREVIOUS_MENU, ACTION_NAV_BACK)):
             self.close()
-
 
     def onClick(self, controlID):
 
@@ -206,10 +195,10 @@ class service_selection(xbmcgui.WindowXMLDialog):
             selItem = self.name_list.getSelectedItem().getLabel()
             # s_entry is a comma seperated string of the services, this changes it into a list
             s_entry = self.name_list.getSelectedItem().getLabel2().split(',')
-            clean_name = selItem.replace(lang(32003),'').replace(lang(32004),'').replace(lang(32005),'').replace('\n','')
+            clean_name = selItem.replace(lang(32003), '').replace(lang(32004), '').replace(lang(32005), '').replace('\n', '')
 
             item_tup = (clean_name, s_entry)
-            
+
             if selItem.endswith(lang(32003)) or selItem.endswith(lang(32004)):
 
                 # if the item is currently enabled or running
@@ -232,7 +221,6 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
             self.update_checklist()
 
-
     def update_checklist(self):
 
         if not self.initiants and not self.finitiants:
@@ -252,10 +240,6 @@ class service_selection(xbmcgui.WindowXMLDialog):
 
         self.getControl(1102).setText(todo_list)
 
-
     def process(self):
 
         self.garcon.process_user_changes(self.initiants, self.finitiants)
-        
-
-
