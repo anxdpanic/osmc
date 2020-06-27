31e9edeeaba2871a4250966d1454b3f54fb6127c
anxdpanic <anxdpanic@gmail.com>
2020-06-27 09:34:11 -0400

    [package][mediacenter-addon-osmc][script.module.osmcsetting.template] Removed

--- package/mediacenter-addon-osmc/build.sh
+++ package/mediacenter-addon-osmc/build.sh
@@ -9,5 +9,4 @@ echo -e "Building package mediacenter-addon-osmc"
 make clean
 mkdir -p files/usr/share/kodi/addons
 cp -ar src/* files/usr/share/kodi/addons
-rm -rf files/usr/share/kodi/addons/script.module.osmcsetting.template*
 dpkg_build files/ mediacenter-addon-osmc.deb
