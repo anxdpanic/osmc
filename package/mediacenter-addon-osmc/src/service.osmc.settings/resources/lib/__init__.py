2836517c9657cc63a671d8f1f26540775a1ffcf4
anxdpanic <anxdpanic@gmail.com>
2020-06-29 10:32:45 -0400

    [package][mediacenter-addon-osmc][service.osmc.settings] fixup extension point path

---
+++ package/mediacenter-addon-osmc/src/service.osmc.settings/resources/lib/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2014-2020 OSMC (KodeKarnage)
+
+    This file is part of service.osmc.settings
+
+    SPDX-License-Identifier: GPL-2.0-only
+    See LICENSES/GPL-2.0-only for more information.
+"""
+
+__all__ = ['default', 'service', 'osmcsettings']
