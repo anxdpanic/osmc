0b3b5ea3e4b456986cf4bda76b143f4786cb215a
anxdpanic <anxdpanic@gmail.com>
2020-07-03 14:56:56 -0400

    [package][mediacenter-addon-osmc] update build scripts

--- package/mediacenter-addon-osmc/build.sh
+++ package/mediacenter-addon-osmc/build.sh
@@ -9,4 +9,16 @@ echo -e "Building package mediacenter-addon-osmc"
 make clean
 mkdir -p files/usr/share/kodi/addons
 cp -ar src/* files/usr/share/kodi/addons
+
+if [ "$1" == "py3" ]
+then
+  install_package "xmlstarlet"
+  verify_action
+
+  for d in ./files/usr/share/kodi/addons/*/ ; do
+    xmlstarlet ed -L -u '/addon/requires/import[@addon="xbmc.python"]/@version' -v "3.0.0" "${d}addon.xml"
+  done
+
+fi
+
 dpkg_build files/ mediacenter-addon-osmc.deb
